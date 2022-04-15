4afdc43583bc083518ce7456a7d7e7b88489a84e
gerpan <nhaths4701@gmail.com>
2022-04-15 18:05:41 +0700

    Update setting screen

--- src/screens/settings/index.tsx
+++ src/screens/settings/index.tsx
@@ -4,6 +4,8 @@ import {Button, Layout, Text, Toggle} from '@ui-kitten/components';
 import {selectThemeType, setThemeType} from '@/features/app/app.slice';
 import {useAppDispatch, useAppSelector} from '@/hooks/redux.hook';
 import {authActions} from '@/features/auth/auth.slice';
+import SettingsTopBar from './components/settings-top-bar';
+import {StyleSheet, View} from 'react-native';
 
 const SettingsScreen = () => {
   const dispatch = useAppDispatch();
@@ -15,19 +17,48 @@ const SettingsScreen = () => {
   };
 
   return (
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text category="h1">Settings</Text>
-      <Toggle
-        checked={themeType === 'dark'}
-        onChange={() =>
-          dispatch(setThemeType(themeType === 'light' ? 'dark' : 'light'))
-        }>
-        {themeType === 'light' ? 'Light theme' : 'Dark theme'}
-      </Toggle>
+    <Layout style={styles.container}>
+      <SettingsTopBar />
+
+      <View style={{flex: 1, width: '100%'}}>
+        <View style={styles.itemContainer}>
+          <Text category="h6">Theme</Text>
+          <Toggle
+            checked={themeType === 'dark'}
+            onChange={() =>
+              dispatch(setThemeType(themeType === 'light' ? 'dark' : 'light'))
+            }>
+            {themeType === 'light' ? 'Light' : 'Dark'}
+          </Toggle>
+        </View>
+
+        <View style={styles.itemContainer}>
+          <Text category="h6">Help</Text>
+        </View>
+
+        <View style={styles.itemContainer}>
+          <Text category="h6">About</Text>
+        </View>
+      </View>
 
       <Button onPress={onPressLogoutButton}>Logout</Button>
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingBottom: 12,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    width: '100%',
+    justifyContent: 'space-between',
+    marginVertical: 12,
+  },
+});
 
 export default SettingsScreen;
